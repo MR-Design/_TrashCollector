382cd34d5965beb4246f2c45e70a163d4f1f4b42
MR-Design <two7city@gmail.comMR-Design>
2018-10-25 22:15:34 -0500

    Initial commit

---
+++ _TrashCollector_DCC/_TrashCollector_DCC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_TrashCollector_DCC.MvcApplication" Language="C#" %>
---
+++ _TrashCollector_DCC/_TrashCollector_DCC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_TrashCollector_DCC.Startup))]
+namespace _TrashCollector_DCC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
